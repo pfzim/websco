30f72dd402b4eab1839480fb3ee2d4765c363be3
Dmitry V. Zimin <DVlZimin@greenatom.ru>
2021-09-22 19:15:41 +0300

    Fixed misprint

--- install.php
+++ install.php
@@ -1191,6 +1191,76 @@ EOT;
 			@-ms-viewport {
 				width: device-width
 			}
+			
+/*--------- SLIDER --------*/			
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 90px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ca2222;
+  -webkit-transition: .4s;
+  transition: .4s;
+   border-radius: 34px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: #2ab934;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(55px);
+}
+
+.slider:after
+{
+ content:'OFF';
+ color: white;
+ display: block;
+ position: absolute;
+ transform: translate(-50%,-50%);
+ top: 50%;
+ left: 50%;
+ font-size: 10px;
+ font-family: Verdana, sans-serif;
+}
+
+input:checked + .slider:after
+{  
+  content:'ON';
+}
+
+/*--------- END --------*/			
 		</style>
 		<script type="text/javascript">
 			function gi(name)
@@ -1456,7 +1526,7 @@ EOT;
 			<div class="form-group">
 				<label for="use_gssapi" class="control-label col-sm-2">Use GSSAPI for LDAP and SCORCH:</label>
 				<div class="col-sm-5">
-					<input id="use_gssapi" name="use_gssapi" class="form-control" type="checkbox" value="1" />
+					<label class="switch"><input id="use_gssapi" name="use_gssapi" class="form-control" type="checkbox" value="1" /><div class="slider round"></div></label>
 				</div>
 			</div>
 			<div class="form-group">
