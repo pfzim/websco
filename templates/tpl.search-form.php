19b33675728ce6c7f710ab76ece3e5869cc0b481
Dmitry V. Zimin <DVlZimin@greenatom.ru>
2021-10-22 12:18:21 +0300

    Update description

--- templates/tpl.search-form.php
+++ templates/tpl.search-form.php
@@ -1,6 +1,6 @@
 <?php if(!defined('Z_PROTECTED')) exit; ?>
 
 		<form action="<?php ln('runbooks_search'); ?>" method="post">
-			<?php L('FindRunbook') ?>: <input type="text" id="search" name="search" class="form-field" placeholder="Runbook name" value="<?php if(isset($search)) eh($search); ?>">
+			<?php L('FindRunbook') ?>: <input type="text" id="search" name="search" class="form-field" placeholder="<?php L('RunbookName') ?>..." value="<?php if(isset($search)) eh($search); ?>">
 			<input class="button-other" type="submit" value="<?php L('Search') ?>" /><br />
 		</form>
