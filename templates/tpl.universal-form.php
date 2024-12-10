edfcde986b403a24efe46eaae328374a16a63d99
Dmitry V. Zimin <pfzim@mail.ru>
2024-12-10 11:58:46 +0300

    Define a link target

--- templates/tpl.universal-form.php
+++ templates/tpl.universal-form.php
@@ -6,7 +6,7 @@
 				<span class="close" onclick="this.parentNode.parentNode.style.display='none'">&times;</span>
 				<h3 id="uform-title" class='form-header'></h3>
 				<div id="uform-description" class='form-description'></div>
-				<div id="uform-url" class='form-url'><a id="uform-url-href" href="#"><?php L('Instruction') ?></a></div>
+				<div id="uform-url" class='form-url'><a id="uform-url-href" href="#" target="_blank"><?php L('Instruction') ?></a></div>
 				<form id="uform-fields" class="form-fields">
 				</form>
 			</div>
