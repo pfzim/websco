1853a7c2d627c1b6ae31744cc6d553b451ab7505
Dmitry V. Zimin <pfzim@mail.ru>
2025-02-27 12:15:33 +0300

    Minor fixes

--- routes/custom.php
+++ routes/custom.php
@@ -16,7 +16,31 @@ function custom(&$core, $params, $post_data)
 		include(TEMPLATES_DIR.'tpl.message.php');
 		return;
 	}
-	
+
+	$current_folder = array(
+		'name' => LL('RootLevel'),
+		'id' => 0,
+		'pid' => '00000000-0000-0000-0000-000000000000',
+		'guid' => '00000000-0000-0000-0000-000000000000',
+		'childs' => NULL
+	);
+
+	if($core->db->select_assoc_ex($folder, rpv('SELECT f.`id`, f.`pid`, f.`guid`, f.`name` FROM @runbooks_folders AS f WHERE f.`id` = !', $runbook['folder_id'])))
+	{
+		if(!empty($folder[0]['name']))
+		{
+			$current_folder = array(
+				'name' => $folder[0]['name'],
+				'id' => $folder[0]['id'],
+				'pid' => $folder[0]['pid'],
+				'guid' => $folder[0]['guid'],
+				'childs' => NULL
+			);
+		}
+	}
+
+	$folders_tree = $core->Runbooks->get_folders_tree(TRUE);
+
 	$custom_script_dir = ROOT_DIR.'custom'.DIRECTORY_SEPARATOR.preg_replace('/[^a-z0-9_-]/i', '', $runbook['guid']).DIRECTORY_SEPARATOR;
 	$custom_script_file = $custom_script_dir.'main.php';
 	if(!file_exists($custom_script_file))
